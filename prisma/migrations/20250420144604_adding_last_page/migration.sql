34a6ebadb7e118039c181ba93ba161f6e167458c
megas-mdn <mdn.melqui@gmail.com>
2025-04-20 11:46:48 -0300

    feat: Migrate lastpage colunm

---
+++ prisma/migrations/20250420144604_adding_last_page/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Card" ADD COLUMN     "lastPage" INTEGER DEFAULT 0;
--- src/validations/Card/createCardSchema.ts
+++ src/validations/Card/createCardSchema.ts
@@ -5,6 +5,7 @@ export const createCardSchema = z.object({
   imageUrl: z.string().min(1, "Image URL is required").optional(),
   lastViewed: z.string().optional(),
   order: z.number().min(1, "Order must be greater than 0"),
+  lastPage: z.number().min(1, "Order must be greater than 0").optional(),
 });
 
 export type TCreateCard = z.infer<typeof createCardSchema>;
